40e4ba3d2f2ccbe879be496a36af195921ac4020
Trinity Quirk <tquirk@ymb.net>
2016-03-13 10:37:26 -0500

    Added comment blocks to shader files
    The shader header and implementation files didn't have our standard
    comment blocks, so we have added them.  Also, there were a couple
    of strings in shader.cc which we added our l10n _() around.

--- shader.cc
+++ shader.cc
@@ -1,3 +1,32 @@
+/* shader.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 13 Mar 2016, 10:36:56 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the function definitions for OpenGL shader and
+ * program handling.
+ *
+ * Things to do
+ *
+ */
+
 #include <string.h>
 
 #include <string>
@@ -45,7 +74,7 @@ GLuint create_shader(GLenum type, const std::string& src)
 
     if (shader == 0)
     {
-        s << "could not create shader: " << GLenum_to_string(glGetError());
+        s << _("could not create shader: ") << GLenum_to_string(glGetError());
         throw std::runtime_error(s.str());
     }
 
@@ -88,7 +117,8 @@ GLuint create_program(GLuint vert, GLuint geom, GLuint frag, const char *out)
 
     if (pgm == 0)
     {
-        s << "could not create GL program: " << GLenum_to_string(glGetError());
+        s << _("could not create GL program: ")
+          << GLenum_to_string(glGetError());
         throw std::runtime_error(s.str());
     }
 
@@ -101,7 +131,7 @@ GLuint create_program(GLuint vert, GLuint geom, GLuint frag, const char *out)
         glAttachShader(pgm, frag);
     if ((err = glGetError()) != GL_NO_ERROR)
     {
-        s << "Could not attach shaders to the shader program: "
+        s << _("Could not attach shaders to the shader program: ")
           << GLenum_to_string(err);
         glDeleteProgram(pgm);
         throw std::runtime_error(s.str());
--- shader.h
+++ shader.h
@@ -1,3 +1,34 @@
+/* shader.h                                                -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 13 Mar 2016, 10:34:49 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the shader handling prototypes.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_SHADER_H__
+#define __INC_R9_SHADER_H__
+
 #include <string>
 
 #include <GL/gl.h>
@@ -6,3 +37,5 @@ GLuint load_shader(GLenum, const std::string&);
 GLuint create_shader(GLenum, const std::string&);
 GLuint create_program(GLuint, GLuint, GLuint, const char *);
 std::string GLenum_to_string(GLenum);
+
+#endif /* __INC_R9_SHADER_H__ */
