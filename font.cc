3ee4c7f0ce83f66c8d2ce977088d19ac5b9f0c0e
Trinity Quirk <tquirk@ymb.net>
2016-05-22 09:18:55 -0500

    Make FT lib singleton stuff static
    The Freetype singleton and its reference count should be static,
    so we won't be able to muck with them from outside.
    
    Also added the standard comment header.

--- font.cc
+++ font.cc
@@ -1,3 +1,36 @@
+/* font.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 22 May 2016, 09:18:29 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the implementation for the font handling, via
+ * the Freetype library.
+ *
+ * We include a static Freetype library singleton with reference
+ * counting in here, so that we don't have to think about the library
+ * anywhere else.
+ *
+ * Things to do
+ *
+ */
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -12,8 +45,8 @@
  * be handled within the same thread, so no multi-thread weirdness
  * should intrude.
  */
-FT_Library *ft_lib = NULL;
-int ft_lib_count = 0;
+static FT_Library *ft_lib = NULL;
+static int ft_lib_count = 0;
 
 static FT_Library *init_freetype(void)
 {
