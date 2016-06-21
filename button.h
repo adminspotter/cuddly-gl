683aa3d09f9d7c1f3fb4d247f3eda5c53e4f2def
Trinity Quirk <tquirk@ymb.net>
2016-06-21 16:23:10 -0500

    Added header for button class
    The button will be a very thin shim on top of the existing label,
    adding only some standard callback functions.  They'll need to be
    static, since we can't pass funcptrs to object methods.

---
+++ button.h
@@ -0,0 +1,53 @@
+/* button.h                                                -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 21 Jun 2016, 15:21:32 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the basic button object declaration.  We derive
+ * from the label, which will take care of the actual text/image.  In
+ * this class, we'll actually implement some event callbacks.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_BUTTON_H__
+#define __INC_R9_BUTTON_H__
+
+#include <list>
+
+#include "label.h"
+
+namespace ui
+{
+    class button : public label
+    {
+      protected:
+        static void activate(panel *, void *, void *);
+        static void deactivate(panel *, void *, void *);
+        static void click(panel *, void *, void *);
+
+      public:
+        button(context *, GLuint = 0, GLuint = 0);
+        virtual ~button();
+    };
+}
+
+#endif /* __INC_R9_BUTTON_H__ */
