66aa91e99d293f70ce811850479a85c694acd5fb
Trinity Quirk <tquirk@ymb.net>
2016-08-07 12:43:41 -0500

    Factor ui::image into its own include file
    The ui::image is getting a bit more complex, and is needed in a
    few other places, so we'll go ahead and split it out into its own
    file, so we don't need to include stuff that may not make sense.

---
+++ image.h
@@ -0,0 +1,78 @@
+/* font.h                                                  -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 07 Aug 2016, 12:43:16 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_IMAGE_H__
+#define __INC_R9_IMAGE_H__
+
+#include <string.h>
+
+#include <GL/gl.h>
+
+namespace ui
+{
+    struct image
+    {
+        unsigned char *data;
+        GLuint width, height, per_pixel;
+
+        image()
+            {
+                this->width = 0;
+                this->height = 0;
+                this->per_pixel = 0;
+                this->data = NULL;
+            };
+        image(const image& i)
+            {
+                this->data = NULL;
+                *this = i;
+            };
+        ~image()
+            {
+                if (this->data != NULL)
+                    delete[] this->data;
+            };
+        image& operator=(const image& i)
+            {
+                GLuint new_size = i.width * i.height * i.per_pixel;
+
+                if (this->data != NULL)
+                    delete[] this->data;
+                this->width = i.width;
+                this->height = i.height;
+                this->per_pixel = i.per_pixel;
+                if (new_size > 0)
+                {
+                    this->data = new unsigned char[new_size];
+                    memcpy(this->data, i.data, new_size);
+                }
+                return *this;
+            };
+    };
+
+}
+
+#endif /* __INC_R9_IMAGE_H__ */
