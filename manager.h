c75f33d59b535720fd56d352ad6c15395a6de102
Trinity Quirk <tquirk@ymb.net>
2016-08-10 08:20:22 -0500

    Add the new manager header
    The manager widget combines the visual capabilities of the panel
    with the child management of the composite.  This is the precursor
    to our user-level manager widgets such as the bulletin board and
    row-column.
    
    Re: issue r9#78

---
+++ manager.h
@@ -0,0 +1,47 @@
+/* manager.h                                          -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 10 Aug 2016, 08:20:01 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the declaration for the manager object.  It
+ * combines the panel with the composite, so we can have dialog boxes
+ * and menu bars and menus and whatnot.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_MANAGER_H__
+#define __INC_R9_MANAGER_H__
+
+#include "panel.h"
+#include "composite.h"
+
+namespace ui
+{
+    class manager : public panel, public composite
+    {
+      public:
+        manager(composite *, GLuint, GLuint);
+        ~manager();
+    };
+}
+
+#endif /* __INC_R9_MANAGER_H__ */
