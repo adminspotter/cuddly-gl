4f15133bcde7c1213c5786d623af098e0072a8ba
Trinity Quirk <tquirk@ymb.net>
2016-08-14 09:05:09 -0500

    Repair mouse click callback error in manager
    There is a difference between what the mouse callback handlers
    expect and what the callback routines themselves expect.  We were
    not taking this into account in our button callback, so everything
    was being treated as a button-down event.  The same was true with
    the key callback.

--- manager.cc
+++ manager.cc
@@ -1,6 +1,6 @@
 /* manager.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 14 Aug 2016, 08:21:15 tquirk
+ *   last updated 14 Aug 2016, 09:08:01 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -85,7 +85,9 @@ void ui::manager::button_callback(panel *p, void *call, void *client)
     {
         ui::btn_call_data *bcd = (ui::btn_call_data *)call;
 
-        m->mouse_btn_callback(bcd->button, bcd->state);
+        m->mouse_btn_callback(bcd->button,
+                              (bcd->state == ui::callback::btn_up
+                               ? ui::mouse::up : ui::mouse::down));
     }
 }
 
@@ -97,7 +99,10 @@ void ui::manager::keypress_callback(panel *p, void *call, void *client)
     {
         ui::key_call_data *kcd = (ui::key_call_data *)call;
 
-        m->key_callback(kcd->key, kcd->character, kcd->state, kcd->mods);
+        m->key_callback(kcd->key, kcd->character,
+                        (kcd->state == ui::callback::key_up
+                         ? ui::key::up : ui::key::down),
+                        kcd->mods);
     }
 }
 
