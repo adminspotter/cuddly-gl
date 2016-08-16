7f9ef4ab623cc97b5c3f9df4128e69f3996b3ee5
Trinity Quirk <tquirk@ymb.net>
2016-08-15 21:54:16 -0500

    Repair pixel size retrieval in text field
    We added methods to retrieve the correct pixel size for purposes of
    scaling our children properly, and the text field had not been
    updated to use the new methods.

--- text_field.cc
+++ text_field.cc
@@ -1,6 +1,6 @@
 /* text_field.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 09 Aug 2016, 09:08:54 tquirk
+ *   last updated 15 Aug 2016, 19:19:29 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -231,10 +231,9 @@ void ui::text_field::generate_cursor(int pixel_pos)
         if (pixel_pos < 0)
             pixel_pos = this->get_cursor_pixel_pos();
 
-        this->parent->get(ui::element::size, ui::size::width, &temp);
-        pw = 2.0f / (float)temp;
-        this->parent->get(ui::element::size, ui::size::height, &temp);
-        ph = -2.0f / (float)temp;
+        this->parent->get(ui::element::pixel_size, ui::size::width, &pw);
+        this->parent->get(ui::element::pixel_size, ui::size::height, &ph);
+        ph = -ph;
         sw = pw * (float)pixel_pos;
         m[0] = this->margin[0] * ph;  b[0] = this->border[0] * ph;
         m[1] = this->margin[1] * pw;  b[1] = this->border[1] * pw;
