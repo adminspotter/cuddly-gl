5ea608bdfac4cdc0d498e49d98c3c9cdc67c2120
Trinity Quirk <tquirk@ymb.net>
2016-08-21 12:25:45 -0500

    Add password field header
    We've decided to subclass the text field for the password entry
    field, rather than have a flag.  This adds the header file for
    the subclass.
    
    Re: issue r9#104

---
+++ password.h
@@ -0,0 +1,45 @@
+/* password.h                                              -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 21 Aug 2016, 12:25:33 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the password field declaration.  We're
+ * subclassing the text_field, since it already does most of what we
+ * want here.  We'll display a set of asterisks, rather than the
+ * actual string.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_PASSWORD_H__
+#define __INC_R9_PASSWORD_H__
+
+namespace ui
+{
+    class password : public text_field
+    {
+      public:
+        password(composite *, GLuint, GLuint);
+        virtual ~password();
+    };
+}
+
+#endif /* __INC_R9_PASSWORD_H__ */
