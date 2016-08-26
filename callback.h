fdc4a17b8d0e07a57476069d7be590e015fbf393
Trinity Quirk <tquirk@ymb.net>
2016-08-25 22:52:34 -0500

    Start the callback role class
    We need to factor out the callback lists, so they can be used in
    any other class.  The lists themselves are pretty minimal, though
    this may combine the lists themselves and the dispatcher from the
    composite.  We shall see what works best.

---
+++ callback.h
@@ -0,0 +1,41 @@
+/* callback.h                                              -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 25 Aug 2016, 22:50:53 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the basic callback role.  We need to factor the
+ * callback lists out of everything, so they can be a role in
+ * anything.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_CALLBACK_H__
+#define __INC_R9_CALLBACK_H__
+
+namespace ui
+{
+    class callback
+    {
+    };
+}
+
+#endif /* __INC_R9_CALLBACK_H__ */
