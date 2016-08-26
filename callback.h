4beff262a480bfb79a26875627beab1005b089ec
Trinity Quirk <tquirk@ymb.net>
2016-08-25 23:05:42 -0500

    Copy most of the panel's callbacks into the callback
    Most of the necessary stuff for callback lists is copied into the
    class.  It's also now called the event_handler, because there is a
    collision with the ui::callback namespace in ui_defs.h.

--- callback.h
+++ callback.h
@@ -1,6 +1,6 @@
 /* callback.h                                              -*- C++ -*-
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 25 Aug 2016, 22:50:53 tquirk
+ *   last updated 25 Aug 2016, 23:05:19 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -31,10 +31,51 @@
 #ifndef __INC_R9_CALLBACK_H__
 #define __INC_R9_CALLBACK_H__
 
+#include <list>
+
+#include "panel.h"
+
 namespace ui
 {
-    class callback
+    /* A forward declaration, to solve multi-include problems */
+    class panel;
+
+    /* Callback function pointer */
+    typedef void (*cb_fptr)(panel *, void *, void *);
+
+    typedef struct cb_list_tag
     {
+        cb_fptr ptr;
+        void *client_data;
+
+        bool operator==(const struct cb_list_tag& p) const
+            {
+                return (this->ptr == p.ptr
+                        && this->client_data == p.client_data);
+            };
+        void operator()(panel *p, void *call_data)
+            {
+                this->ptr(p, call_data, this->client_data);
+            };
+    }
+    cb_list_elem;
+
+    class event_handler
+    {
+      protected:
+        std::list<cb_list_elem> enter_cb, leave_cb, motion_cb;
+        std::list<cb_list_elem> btn_down_cb, btn_up_cb;
+        std::list<cb_list_elem> key_down_cb, key_up_cb;
+
+        std::list<cb_list_elem>& which_cb_list(GLuint);
+
+      public:
+        event_handler();
+        ~event_handler();
+
+        virtual void add_callback(GLuint, cb_fptr, void *);
+        virtual void remove_callback(GLuint, cb_fptr, void *);
+        virtual void call_callbacks(GLuint, void *);
     };
 }
 
