57fc61894ae0dc30ad05cd4eec1b0d685b31b350
Trinity Quirk <tquirk@ymb.net>
2016-09-07 06:59:16 -0500

    Add header for multi-line label
    For issue r9#80, we want to have a label which will span multiple
    lines, and do line breaks and things.
    
    We want to keep this functionality separate, because the button
    and text field don't need this.  If we want to have a text area, we
    can revisit the decision.

---
+++ multi_label.h
@@ -0,0 +1,47 @@
+/* multi_label.h                                           -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 07 Sep 2016, 06:57:19 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the multi-line label object declaration.  We
+ * derive from the regular label, and add handling for breaking the
+ * string into multiple lines by whitespace, or by characters if there
+ * are no spaces (no "page-widening posts").
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_MULTI_LABEL_H__
+#define __INC_R9_MULTI_LABEL_H__
+
+#include "label.h"
+
+namespace ui
+{
+    class multi_label : public label
+    {
+      public:
+        multi_label(composite *, GLuint, GLuint);
+        virtual ~multi_label();
+    };
+}
+
+#endif /* __INC_R9_MULTI_LABEL_H__ */
