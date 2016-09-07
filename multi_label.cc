dbc33543c3d513662390757a1ddecb0a1b2c65ff
Trinity Quirk <tquirk@ymb.net>
2016-09-07 07:12:39 -0500

    Add stub implementation of multi-line label
    This adds a (very basic) implementation of the constructor and
    destructor for the multi-line label.  It also adds the appropriate
    stuff so that the files will be compiled.  We're off to the races!
    
    Re: issue r9#80

---
+++ multi_label.cc
@@ -0,0 +1,41 @@
+/* multi_label.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 07 Sep 2016, 07:08:13 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the multi-line label object definitions.  We
+ * derive from the label, which will give us a basic string rendering
+ * framework.  In this class, we'll add calculation of line breaks and
+ * a desired dimension, not unlike the row-column manager widget.
+ *
+ * Things to do
+ *
+ */
+
+#include "multi_label.h"
+
+ui::multi_label::multi_label(ui::composite *p, GLuint w, GLuint h)
+    : ui::label::label(p, w, h)
+{
+}
+
+ui::multi_label::~multi_label()
+{
+}
