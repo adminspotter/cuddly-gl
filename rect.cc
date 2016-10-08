96c46450da9f1d4b019027bfa46713548acd6248
Trinity Quirk <tquirk@ymb.net>
2016-10-08 10:41:58 -0500

    Add stubs for UI rect base class
    This adds the stub methods for the rect class to the UI toolset.
    The contents will follow in subsequent commits.
    
    Re: issue r9#123

---
+++ rect.cc
@@ -0,0 +1,63 @@
+/* rect.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 07 Oct 2016, 08:04:46 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2016  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the rect object definition.  The rect has a
+ * size and the basic get and set methods.
+ *
+ * Things to do
+ *
+ */
+
+#include "rect.h"
+
+int ui::rect::get_size(GLuint t, void *v)
+{
+}
+
+void ui::rect::set_size(GLuint t, void *v)
+{
+}
+
+ui::rect::rect(GLuint w, GLuint h)
+    : dim(w, h)
+{
+}
+
+ui::rect::~rect()
+{
+}
+
+int ui::rect::get(GLuint e, GLuint t, void *v)
+{
+}
+
+void ui::rect::set(GLuint e, GLuint t, void *v)
+{
+}
+
+void ui::rect::get_va(GLuint e, GLuint t, void *v, ...)
+{
+}
+
+void ui::rect::set_va(GLuint e, GLuint t, void *v, ...)
+{
+}
