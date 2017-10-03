59d167dbcf958a7bfce2cb0e2d84395aabaeef20
Trinity Quirk <tquirk@ymb.net>
2017-10-02 20:48:07 -0500

    Add test for rect size setting
    Likewise with getting the size of a rect object, we can set the
    values with either the set_size or set methods, and can set either
    single values or vectors.

--- test/t_rect.cc
+++ test/t_rect.cc
@@ -100,11 +100,64 @@ void test_get_size(void)
     }
 }
 
+void test_set_size(void)
+{
+    std::string test = "set_size: ", st;
+    test_rect *r = NULL;
+    GLuint dim = 42;
+    glm::ivec2 sz = {6, 5};
+
+    try
+    {
+        r = new test_rect(9, 87);
+    }
+    catch (...)
+    {
+        fail(test + "constructor exception");
+        return;
+    }
+
+    st = "set_size single: ";
+    r->set_size(ui::size::height, &dim);
+    is(r->dim.y, 42, test + st + "expected height");
+
+    st = "set_size vector: ";
+    r->set_size(ui::size::all, &sz);
+    is(r->dim.x, 6, test + st + "expected x-dim");
+    is(r->dim.y, 5, test + st + "expected y-dim");
+
+    st = "set_size bad element subtype: ";
+    dim = 9876;
+    r->set_size(-1, &dim);
+    is(r->dim.x, 6, test + st + "x-dim unchanged");
+    is(r->dim.y, 5, test + st + "y-dim unchanged");
+
+    st = "set single: ";
+    dim = 42;
+    r->set(ui::element::size, ui::size::width, &dim);
+    is(r->dim.x, 42, test + st + "expected width");
+
+    st = "set bad element type: ";
+    dim = 9876;
+    r->set(-1, ui::size::width, &dim);
+    isnt(r->dim.x, 9876, test + st + "width unchanged");
+
+    try
+    {
+        delete r;
+    }
+    catch (...)
+    {
+        fail(test + "destructor exception");
+    }
+}
+
 int main(int argc, char **argv)
 {
-    plan(13);
+    plan(20);
 
     test_create_delete();
     test_get_size();
+    test_set_size();
     return 0;
 }
