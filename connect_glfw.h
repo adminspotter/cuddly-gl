1f6e9c94d5bce4fa752175b8618698c732c5abc5
Trinity Quirk <tquirk@ymb.net>
2017-11-13 07:07:21 -0600

    Add copyright notices to GLFW connector files
    All our source files need to have copyright notices.  These are our
    standard block, with GPL2 notice.

--- connect_glfw.h
+++ connect_glfw.h
@@ -1,3 +1,32 @@
+/* connect_glfw.h                                          -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 13 Nov 2017, 07:07:06 tquirk
+ *
+ * CuddlyGL OpenGL widget toolkit
+ * Copyright (C) 2017  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains function prototypes for connecting the CuddlyGL
+ * widget toolkit to the GLFW windowing system.
+ *
+ * Things to do
+ *
+ */
+
 #ifndef __INC_CONNECT_GLFW_H__
 #define __INC_CONNECT_GLFW_H__
 
