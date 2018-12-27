eec1bdf61e0602b8ec32c52f115651f0aac2ffae
Trinity Quirk <tquirk@ymb.net>
2018-12-27 10:45:22 -0600

    Start adding the armable class
    When we started looking at adding a toggle button class, we found
    that we needed the stuff out of the label for both buttons, but
    that the toggle button needed some of the things that the regular
    button provided, but not some of the other things.  Notably, we
    needed the armed/active states in both, but the behaviour of the
    regular button in its border-changing was not what we wanted in
    the toggle button.
    
    So the armable was born.  For now, it merely contains the armed
    and active members, and does nothing with them.  This is the first
    step on a refactoring path that will take a handful of commits.
    
    Once we get a bit more of the contents of the button moved into
    the armable class, we'll insert it into the hierarchy.  Then we'll
    be able to derive from it for the toggle button.

---
+++ armable.cc
@@ -0,0 +1,46 @@
+/* armable.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 26 Dec 2018, 22:36:52 tquirk
+ *
+ * CuddlyGL OpenGL widget toolkit
+ * Copyright (C) 2018  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the armable object definitions.  This is an
+ * intermediate class between the label and the various buttons.
+ *
+ * Things to do
+ *
+ */
+
+#include "armable.h"
+
+void ui::armable::init(ui::composite *c)
+{
+    this->activated = false;
+    this->armed = false;
+}
+
+ui::armable::armable(ui::composite *c)
+    : ui::label::label(c), ui::active::active(0, 0), ui::rect::rect(0, 0)
+{
+    this->init(c);
+}
+
+ui::armable::~armable()
+{
+}
