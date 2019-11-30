40706a91cb66d91894fc21bf32243727bbc896cb
Trinity Quirk <tquirk@ymb.net>
2019-11-29 22:38:17 -0800

    Add second and third groups to string utils test
    Test the ranges from 0x80 - 0x7ff and 0x800 - 0xffff.

--- test/t_util.cc
+++ test/t_util.cc
@@ -16,9 +16,38 @@ void test_basic_string(void)
     ok(str == result, test + "good round trip");
 }
 
+void test_second_group(void)
+{
+    std::string test = "0x80 - 0x7ff: ";
+
+    std::u32string str = { 0x80, 0x100, 0x7ff };
+    std::string converted = ui::u32strtoutf8(str);
+    std::u32string result = ui::utf8tou32str(converted);
+
+    isnt(converted.size(), str.size(), test + "unequal string lengths");
+    is(converted.size(), 6, test + "expected UTF-8 string length");
+    ok(str == result, test + "good round trip");
+}
+
+void test_third_group(void)
+{
+    std::string test = "0x800 - 0xffff: ";
+
+    std::u32string str = { 0x800, 0x1000, 0xffff };
+    std::string converted = ui::u32strtoutf8(str);
+    std::u32string result = ui::utf8tou32str(converted);
+
+    isnt(converted.size(), str.size(), test + "unequal string lengths");
+    is(converted.size(), 9, test + "expected UTF-8 string length");
+    ok(str == result, test + "good round trip");
+}
+
 int main(int argc, char **argv)
 {
-    plan(2);
+    plan(8);
+
     test_basic_string();
+    test_second_group();
+    test_third_group();
     return exit_status();
 }
