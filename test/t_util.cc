6309e6d4f56ae700a90041668476efc00583663f
Trinity Quirk <tquirk@ymb.net>
2019-11-29 22:46:10 -0800

    Add rest of string encoding test cases
    The rest of the groups 0x10000 - 0x1fffff, 0x200000 - 0x3ffffff,
    and 0x4000000 - 7fffffff.

--- test/t_util.cc
+++ test/t_util.cc
@@ -42,12 +42,54 @@ void test_third_group(void)
     ok(str == result, test + "good round trip");
 }
 
+void test_fourth_group(void)
+{
+    std::string test = "0x10000 - 0x1fffff: ";
+
+    std::u32string str = { 0x10000, 0x80000, 0x1fffff };
+    std::string converted = ui::u32strtoutf8(str);
+    std::u32string result = ui::utf8tou32str(converted);
+
+    isnt(converted.size(), str.size(), test + "unequal string lengths");
+    is(converted.size(), 12, test + "expected UTF-8 string length");
+    ok(str == result, test + "good round trip");
+}
+
+void test_fifth_group(void)
+{
+    std::string test = "0x200000 - 0x3ffffff: ";
+
+    std::u32string str = { 0x200000, 0xffffff, 0x3ffffff };
+    std::string converted = ui::u32strtoutf8(str);
+    std::u32string result = ui::utf8tou32str(converted);
+
+    isnt(converted.size(), str.size(), test + "unequal string lengths");
+    is(converted.size(), 15, test + "expected UTF-8 string length");
+    ok(str == result, test + "good round trip");
+}
+
+void test_sixth_group(void)
+{
+    std::string test = "0x4000000 - 0x7fffffff ";
+
+    std::u32string str = { 0x4000000, 0x4ffffff, 0x7fffffff };
+    std::string converted = ui::u32strtoutf8(str);
+    std::u32string result = ui::utf8tou32str(converted);
+
+    isnt(converted.size(), str.size(), test + "unequal string lengths");
+    is(converted.size(), 18, test + "expected UTF-8 string length");
+    ok(str == result, test + "good round trip");
+}
+
 int main(int argc, char **argv)
 {
-    plan(8);
+    plan(17);
 
     test_basic_string();
     test_second_group();
     test_third_group();
+    test_fourth_group();
+    test_fifth_group();
+    test_sixth_group();
     return exit_status();
 }
